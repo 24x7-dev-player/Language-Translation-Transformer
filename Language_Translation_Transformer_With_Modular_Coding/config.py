45338268bd3d5d64aa13e9ef22700089516c31c0
🧐 ᴛᴜꜱʜᴀʀ-ᴘᴇʀꜱᴘᴇᴄᴛɪᴠᴇ <61559891+TUSHAR-PERSPECTIVE@users.noreply.github.com>
2024-06-24 14:05:33 +0530

    commit

--- Language_Translation_Transformer_With_Modular_Coding/config.py
+++ Language_Translation_Transformer_With_Modular_Coding/config.py
@@ -2,7 +2,7 @@
 def get_config():
     return{
         'batch_size': 8,
-        'num_epochs': 1,
+        'num_epochs': 20,
         'lr': 10**-4,
         'seq_len': 350,
         'd_model': 512, # Dimensions of the embeddings in the Transformer. 512 like in the "Attention Is All You Need" paper.
@@ -14,4 +14,4 @@ def get_config():
         'tokenizer_file': 'tokenizer_{0}.json',
         'experiment_name': 'runs/tmodel'
     }
-    
+      
